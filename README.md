387dc655eb6c2d14630bfa3b15067b4af3796288
Jan Killian <jan.killian@gmail.com>
2014-12-09 12:59:27 +0100

    feat: initial release

---
+++ README.md
@@ -0,0 +1,102 @@
+# [Flask-Run](https://github.com/salsita/flask-run) <a href='https://github.com/salsita'><img align='right' title='Salsita' src='https://www.google.com/a/cpanel/salsitasoft.com/images/logo.gif?alpha=1' /></a>
+
+Flask-based web application runner
+
+[![Latest Version](https://pypip.in/version/Flask-Run/badge.svg)]
+(https://pypi.python.org/pypi/Flask-Run/)
+[![Downloads](https://pypip.in/download/Flask-Run/badge.svg)]
+(https://pypi.python.org/pypi/Flask-Run/)
+[![Supported Python versions](https://pypip.in/py_versions/Flask-Run/badge.svg)]
+(https://pypi.python.org/pypi/Flask-Run/)
+[![License](https://pypip.in/license/Flask-Run/badge.svg)]
+(https://pypi.python.org/pypi/Flask-Run/)
+
+
+## Supported Platforms
+
+* [Python](http://www.python.org/) >= 2.7, 3.3
+* [Flask](http://flask.pocoo.org/) >= 0.9
+
+
+## Get Started
+
+Install using [pip](https://pip.pypa.io/) or [easy_install](http://pythonhosted.org/setuptools/easy_install.html):
+```bash
+pip install Flask-Run
+easy_install Flask-Run
+```
+
+## Example:
+
+#### Flask application: `app.py`
+
+```python
+#!/usr/bin/env python
+
+"""Flask-based web application."""
+
+__all__ = 'app'.split()
+
+import flask
+from config import Config
+
+app = None
+
+def create_app(config):
+  app = flask.Flask(__name__)
+
+  app.config.from_object(config)
+
+  return app
+
+if __name__ == '__main__':
+    from flask.ext.run import run
+    run(create_app, Config)
+```
+
+#### Flask configuration: `config.py`
+
+See [sample flask configuration `config.py](https://github.com/salsita/flask-config#flask-configuration-configpy)
+from [flask-config package](https://github.com/salsita/flask-config)
+that supports listing and selecting environment configurations.
+
+#### Usage:
+
+```
+usage: app.py [-h] [-b [HOST|:PORT]] [-r] [-R] [-d] [-D] [-e ENV]
+              [-E [SHOW_ENV]] [-g]
+
+runs Flask-based web application using Python WSGI reference server
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -b [HOST|:PORT], --bind [HOST|:PORT]
+                        bind to HOST:PORT (default: 127.0.0.1:5000)
+  -r, --reload          reload server on code change (default in development)
+  -R, --no-reload       do not reload server on code change
+  -d, --debug           show debugger on exception (default in development)
+  -D, --no-debug        do not show debugger on exception
+  -e ENV, --env ENV     select environment config (default: development)
+  -E [SHOW_ENV], --show-env [SHOW_ENV]
+                        show environment config and exit (*: all, default: development)
+  -g, --gen-key         generate a good secret key and exit
+
+optional environment variables:
+  APP_ENV               select environment configuration
+  DATABASE_URL          sqlalchemy database uri including credentials
+  SECRET_KEY            secret key for signing session cookies
+
+available environment configurations (*: active):
+ *dev | development
+  prod | production
+  qa | test | testing
+  stage | staging
+  try | experimental
+
+```
+
+## Changelog
+
+#### 0.1.0
+
+* Initial release.
---
+++ flask_run.py
@@ -0,0 +1,110 @@
+"""Flask-based web application runner."""
+
+from __future__ import print_function
+
+__all__ = 'run'.split()
+__version__ = '0.1.0'
+
+def run(create_app, config, description=None, args=None, namespace=None, options=None):
+    """Parses commandline options, updates config, creates and runs the application.
+
+    Supports listing and selecting environment configurations if `Flask-Config`_ configuration class is used.
+
+    .. _Flask-Config: http://pypi.python.org/pypi/Flask-Config
+    """
+    import argparse
+
+    class HelpFormatter(
+        # argparse.RawTextHelpFormatter,
+        argparse.RawDescriptionHelpFormatter,
+        # argparse.ArgumentDefaultsHelpFormatter,
+        # argparse.HelpFormatter,
+        ):
+        pass
+
+    # Get configurations data if
+    envName = getattr(config, 'ENV', None)
+    envMap = getattr(config, 'envMap', None)
+    envHelp = getattr(config, 'envHelp', {})
+    envSelectVar = getattr(config, 'envSelectVar', None)
+    configurations = getattr(config, 'configurations', None)
+    envSelectable = callable(getattr(config, 'select', None))
+    envPrintable = config.__str__ is not object.__str__
+
+    parser = argparse.ArgumentParser(
+        description = description or getattr(config, 'description',
+            'runs Flask-based web application using Python WSGI reference server'),
+        epilog = ''.join((
+            '\n\noptional environment variables:\n{}'.format(
+                '\n'.join(sorted('  {:<20}  {}'.format(envMap[key], envHelp.get(key, ''))
+                for key in envMap))) if envMap else '',
+            '\n\navailable environment configurations (*: active):\n{}'.format(
+                '\n'.join(' {}{}'.format(
+                    '*' if envName in c.names else ' ',
+                    ' | '.join(c.names)
+                ) for c in configurations)) if configurations else '',
+        )),
+        formatter_class = HelpFormatter)
+
+    parser.add_argument('-b', '--bind', metavar='[HOST|:PORT]', default='127.0.0.1:5000',
+        help = 'bind to HOST:PORT (default: 127.0.0.1:5000)')
+
+    debug = config.DEBUG
+    debugMsg = ' (default in {})'.format(config.ENV)
+    debugTrueMsg = debugMsg if debug else ''
+    debugFalseMsg = debugMsg if not debug else ''
+
+    parser.add_argument('-r', '--reload', action='store_true', default=debug,
+        help = 'reload server on code change' + debugTrueMsg)
+    parser.add_argument('-R', '--no-reload', action='store_false', dest='reload',
+        help = 'do not reload server on code change' + debugFalseMsg)
+
+    parser.add_argument('-d', '--debug', action='store_true', default=debug,
+        help = 'show debugger on exception' + debugTrueMsg)
+    parser.add_argument('-D', '--no-debug', action='store_false', dest='debug',
+        help = 'do not show debugger on exception' + debugFalseMsg)
+
+    if envSelectable:
+        parser.add_argument('-e', '--env', default=config.ENV,
+            help = 'select environment config (default: {})'.format(config.ENV))
+
+    if envPrintable:
+        if envSelectable:
+            parser.add_argument('-E', '--show-env', nargs='?', const=True,
+                help = 'show environment config and exit ({}default: {})'
+                    .format('*: all, ' if configurations else '', config.ENV))
+        else:
+            parser.add_argument('-E', '--show-env', action='store_true',
+                help = 'show environment config and exit')
+
+    parser.add_argument('-g', '--gen-key', action='store_true',
+        help = 'generate a good secret key and exit')
+
+
+    args = parser.parse_args(args, namespace)
+
+    if args.gen_key:
+        # See http://flask.pocoo.org/docs/0.10/quickstart/#sessions
+        import os
+        key = os.urandom(24)
+        return print(key)
+        # return print('{0}\n{0!r}'.format(key))
+
+    if envSelectable and args.env:
+        config = config.select(args.env)
+
+    if envPrintable and args.show_env:
+        if configurations and args.show_env == '*':
+            print('\n\n'.join('# {}\n{}'.format(' | '.join(c.names), c) for c in configurations))
+        elif args.show_env is True:
+            print(config)
+        else:
+            print(config.select(args.show_env))
+
+        return
+
+    host, port = (args.bind + ':').split(':')[:2]
+    host = host or '127.0.0.1'
+    port = int(port) if port else 5000
+
+    return create_app(config).run(host, port, args.debug, use_reloader=args.reload, **(options or {}))
